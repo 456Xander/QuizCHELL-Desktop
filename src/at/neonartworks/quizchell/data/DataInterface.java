738a80b6ac6f71a5bcb8aab0662097fdabbfbed8
sachregister <clemensl@aon.at>
2016-11-21 14:52:16 +0100

    Edited Interface

--- src/at/neonartworks/quizchell/data/DataInterface.java
+++ src/at/neonartworks/quizchell/data/DataInterface.java
@@ -1,5 +1,13 @@
 package at.neonartworks.quizchell.data;
 
-public class DataInterface {
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+
+public class DataInterface {	
+	
+	public DataInterface() {
+		CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+	}
 
 }
